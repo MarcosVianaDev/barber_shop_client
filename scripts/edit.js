b876811b9276514ce58a0a19909089dcd68ae6cb
MarcosVianaDev <marcos.viana.dev@gmail.com>
2023-09-24 23:56:14 -0300

    + edit file ok

---
+++ scripts/edit.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const url_params = new URLSearchParams(window.location.search)
+  const item_id = url_params.get('id')
+  const data_aba = url_params.get('aba')
+  const form = document.getElementsByTagName('form')
+  var data_cadastro;
+
+  //Verifica se tem id e aba do item para pesquisar no DB
+  if (item_id && data_aba) {
+    fetch(`http://localhost:8080/${data_aba}/${item_id}`)
+      .then(res => res.json())
+      .then((res) => {
+        data_cadastro = res.data_cadastro
+        temp_date = String(data_cadastro).split('/')
+        document.getElementById('data_cadastro').value = `${temp_date[2]}-${temp_date[1]}-${temp_date[0]}`
+        //percorre os elementos INPUT do formulário para preencher com os dados recebidos
+        for (let index = 0; index < form[0].length; index++) {
+          const element = form[0][index];
+          if (element.nodeName != "INPUT") {
+            continue
+          }
+          if (element.type == 'checkbox' && res[element.id] == '1') {
+            element.checked = true
+            continue
+          }
+          element.value = res[element.id] == undefined ? '' : res[element.id]
+        }
+        form[0].onreset = (evt => {
+          window.location.reload()
+        })
+      });
+
+    //Exibe apenas os elementos da classe do mesmo tipo de dado "aba"
+    document.querySelectorAll(`.${data_aba}`).forEach(element => {
+      element.style.display = 'inline-block'
+    })
+  } else { // se não tiver ID e aba, volta para a página inicial
+    alert('Sem dados...')
+    window.location.href = window.location.origin
+  }
+
+  form[0].onsubmit = (evt => {
+    evt.preventDefault()
+    data_send = {}
+    data_send['data_cadastro'] = data_cadastro
+    for (let index = 0; index < evt.target.length; index++) {
+      const element = evt.target[index];
+      if (!element.parentElement.className.includes(data_aba)) {
+        continue
+      }
+      if (element.type == 'checkbox') {
+        data_send[String(element.id).replace('new_', '')] = element.checked ? '1' : '0'
+        continue
+      }
+      if (element.nodeName == 'INPUT') {
+        data_send[String(element.id).replace('new_', '')] = element.value
+      }
+    }
+    console.log(data_send);
+    fetch(`http://localhost:8080/${data_aba}/${item_id}/edit`, {
+      method: 'PUT',
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(data_send),
+    })
+      .then(response => response.json())
+      .then(json => console.log(json));
+    console.log('\nSalvando...');
+    // window.location.href = window.location.origin
+  });
+
+})
