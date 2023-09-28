d3f853afb5811d02a8990a50990d76ed13ded982
MarcosVianaDev <marcos.viana.dev@gmail.com>
2023-09-28 07:21:47 -0300

    URL server https

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 const URL_SERVER = {
     'dev': 'http://localhost:8080',
-    'prod': 'http://barbershop.marcosviana2.repl.co'
+    'prod': 'https://barbershop.marcosviana2.repl.co'
 }
 const URL_IN_USE = URL_SERVER.prod
 
