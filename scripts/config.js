49e55c0dd96c9ff9b8ec876395c197905a79e8ea
Marcos Viana <97001316+Marcosviana91@users.noreply.github.com>
2023-10-08 22:00:13 -0300

    produção

--- scripts/config.js
+++ scripts/config.js
@@ -2,6 +2,6 @@ const URL_SERVER = {
     'dev': 'http://localhost:8080',
     'prod': 'https://barbershop.marcosviana2.repl.co'
 }
-const URL_IN_USE = URL_SERVER.dev
+const URL_IN_USE = URL_SERVER.prod
 
 export { URL_IN_USE }
