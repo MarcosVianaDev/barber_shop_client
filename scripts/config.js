dacb6743da078764df6f2a6e2ce937373f2af15f
MarcosVianaDev <145157797+MarcosVianaDev@users.noreply.github.com>
2023-10-21 09:50:27 -0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,6 @@
 const URL_SERVER = {
     'dev': 'http://localhost:8080',
-    'prod': 'https://barbershop.marcosviana2.repl.co',
-    'home': 'http://201.25.180.86:3377'
+    'prod': 'https://barbershop.marcosviana2.repl.co'
 }
 const URL_IN_USE = URL_SERVER.prod
 
