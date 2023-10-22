bf3bec4f8a09ed8c4a4d25ec6b2a73a2c7d68c8a
MarcosVianaDev <145157797+MarcosVianaDev@users.noreply.github.com>
2023-10-22 07:36:05 -0300

    using home server

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,8 @@
 const URL_SERVER = {
     'dev': 'http://localhost:8080',
-    'prod': 'https://barbershop.marcosviana2.repl.co'
+    'prod': 'https://barbershop.marcosviana2.repl.co',
+    'home': 'http://201.25.180.86'
 }
-const URL_IN_USE = URL_SERVER.prod
+const URL_IN_USE = URL_SERVER.home
 
 export { URL_IN_USE }
